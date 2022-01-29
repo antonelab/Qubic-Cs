0719a5058d399ff432ee44a14048a781bb5a1149
antonelab <nelab0917@gmail.com>
2022-01-29 22:40:30 +0100

    Dodan userControl table4

---
+++ Qubic/table4.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Qubic
+{
+    partial class table4
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l4Form4 = new Qubic.level4Form();
+            this.l4Form3 = new Qubic.level4Form();
+            this.l4Form2 = new Qubic.level4Form();
+            this.l4Form1 = new Qubic.level4Form();
+            this.SuspendLayout();
+            // 
+            // l4Form4
+            // 
+            this.l4Form4.Location = new System.Drawing.Point(0, 680);
+            this.l4Form4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.l4Form4.Name = "l4Form4";
+            this.l4Form4.Size = new System.Drawing.Size(199, 210);
+            this.l4Form4.TabIndex = 0;
+            // 
+            // l4Form3
+            // 
+            this.l4Form3.Location = new System.Drawing.Point(57, 455);
+            this.l4Form3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.l4Form3.Name = "l4Form3";
+            this.l4Form3.Size = new System.Drawing.Size(207, 211);
+            this.l4Form3.TabIndex = 1;
+            // 
+            // l4Form2
+            // 
+            this.l4Form2.Location = new System.Drawing.Point(109, 236);
+            this.l4Form2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.l4Form2.Name = "l4Form2";
+            this.l4Form2.Size = new System.Drawing.Size(202, 205);
+            this.l4Form2.TabIndex = 2;
+            // 
+            // l4Form1
+            // 
+            this.l4Form1.Location = new System.Drawing.Point(161, 14);
+            this.l4Form1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.l4Form1.Name = "l4Form1";
+            this.l4Form1.Size = new System.Drawing.Size(230, 207);
+            this.l4Form1.TabIndex = 3;
+            // 
+            // table4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.Controls.Add(this.l4Form1);
+            this.Controls.Add(this.l4Form2);
+            this.Controls.Add(this.l4Form3);
+            this.Controls.Add(this.l4Form4);
+            this.Name = "table4";
+            this.Size = new System.Drawing.Size(407, 890);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private level4Form l4Form4;
+        private level4Form l4Form3;
+        private level4Form l4Form2;
+        private level4Form l4Form1;
+    }
+}
